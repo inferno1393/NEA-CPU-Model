bc75d04bd36543568af69589fd48054172c84e1a
Rob Brown <rd_brown@btinternet.com>
2023-12-21 10:38:31 +0000

    Updated to pass in dictionary and remove white space

--- NEA CPU Model/Parser.cs
+++ NEA CPU Model/Parser.cs
@@ -13,116 +13,7 @@ namespace NEA_CPU_Model
         // Attributes
         private StackArray<string> splitInstructions;
         private QueueArray<string> instructions;
-
-        // constructor
-        public Parser(QueueArray<string> instructions, StackArray<string> splitInstructions)
-        {
-            this.splitInstructions = splitInstructions;
-            this.instructions = instructions;
-        }
-
-        // returns a string showing the validity of the instructions
-        public string ParseInstructions(QueueArray<string> instructions, StackArray<string> splitInstructions)
-        {
-            // splits the instructions in the queue into Opcode and Operand
-            for (int i = 0; i < instructions.Count; i++)
-            {
-                string instruction = instructions.Dequeue();
-                splitInstructions.Push(GetOpcode(instruction));
-                splitInstructions.Push(GetOperand(instruction));
-
-                instructions.Enqueue(instruction);
-            }
-
-            splitInstructions.Pop(); // pops of the operand of the HALT instruction
-
-            // checks last instruction is a HALT (since program must halt)
-            if (splitInstructions.Pop() != "HALT")
-            {
-                return "Invalid, no HALT command";
-            }
-
-            // checks instructions are valid
-            for (int i = 0; i < splitInstructions.Count; i++)
-            {
-                if (!CheckInstruction(splitInstructions.Pop(), splitInstructions.Pop()))
-                {
-                    return "Invalid, incorrect instruction";
-                }
-            }
-
-            // else the instructions are valid
-            return "Valid";
-        }
-
-        static bool CheckInstruction(string Operand, string Opcode)
-        {
-            // if the Opcode is invalid, the instruction is invalid
-            if (OpcodeFormat(Opcode) == "invalid")
-            {
-                return false;
-            }
-            // if the Operand is invalid, the instruction is invalid
-            else if (OperandFormat(Operand) == "invalid")
-            {
-
-            }
-            // if the Operand is not in the correct format for the Opcode, the instruction is invalid
-            else if (OpcodeFormat(Opcode) != OperandFormat(Operand))
-            {
-                return false;
-            }
-
-            // else the instruction is valid
-            return true;
-        }
-
-        static string OperandFormat(string Operand)
-        {
-            // returns the format the Operand is in by counting the commans present
-            string format = string.Empty;
-            int count = 0;
-
-            for (int i = 0; i < Operand.Length; i++)
-            {
-                if (Operand[i] == ',')
-                {
-                    count++;
-                }
-            }
-
-            if (Operand == " ")
-            {
-                format = " ";
-            }
-            else
-            {
-                switch (count)
-                {
-                    case 0:
-                        format = "<label>";
-                        break;
-                    case 1:
-                        format = "Rd, <memory ref>";
-                        break;
-                    case 2:
-                        format = "Rd, Rn, <memory ref>";
-                        break;
-                    default:
-                        format = "invalid";
-                        break;
-                }
-            }
-
-            return format;
-        }
-
-        static string OpcodeFormat(string Opcode)
-        {
-            // uses a dictionary to return the format the operand should be in
-            string format = string.Empty;
-
-            Dictionary<string, string> instructionSet = new()
+        private Dictionary<string, string> instructionSet = new()
             {
                 { "LDR", "Rd, <memory ref>" },
                 { "STR", "Rd, <memory ref>" },
@@ -144,16 +35,118 @@ namespace NEA_CPU_Model
                 {"HALT", " " }
             };
 
-            if (instructionSet.ContainsKey(Opcode))
+
+        // constructor
+        public Parser(QueueArray<string> instructions, StackArray<string> splitInstructions)
+        {
+            this.splitInstructions = splitInstructions;
+            this.instructions = instructions;
+        }
+
+        // returns a string showing the validity of the instructions
+        public string ParseInstructions(QueueArray<string> instructions, StackArray<string> splitInstructions, Dictionary<string,string> instructionSet)
+        {
+            // splits the instructions in the queue into Opcode and Operand
+            for (int i = 0; i < instructions.Count; i++)
             {
-                format = instructionSet[Opcode];
+                string instruction = instructions.Dequeue();
+
+                instruction = instruction.Replace(" ", ""); // removes white space from the instruction
+
+                splitInstructions.Push(GetOpcode(instruction));
+                splitInstructions.Push(GetOperand(instruction));
+
+                instructions.Enqueue(instruction);
+            }   
+
+            splitInstructions.Pop(); // pops of the operand of the HALT instruction
+
+            // checks last instruction is a HALT (since program must halt)
+            if (splitInstructions.Pop() != "HALT")
+            {
+                return "Invalid, no HALT command";
+            }
+
+            // checks instructions are valid
+            for (int i = 0; i < splitInstructions.Count; i++)
+            {
+                if (!CheckInstruction(splitInstructions.Pop(), splitInstructions.Pop(), instructionSet))
+                {
+                    return "Invalid, incorrect instruction";
+                }
+            }
+
+            // else the instructions are valid
+            return "Valid";
+        }
+
+        static bool CheckInstruction(string Operand, string Opcode, Dictionary<string,string> instructionSet)
+        {
+            // if the Opcode is invalid, the instruction is invalid
+            if (OpcodeFormat(Opcode, instructionSet) == "invalid")
+            {
+                return false;
+            }
+            // if the Operand is invalid, the instruction is invalid
+            else if (OperandFormat(Operand) == "invalid")
+            {
+
+            }
+            // if the Operand is not in the correct format for the Opcode, the instruction is invalid
+            else if (OpcodeFormat(Opcode, instructionSet) != OperandFormat(Operand))
+            {
+                return false;
+            }
+
+            // else the instruction is valid
+            return true;
+        }
+
+        static string OperandFormat(string Operand)
+        {
+            // returns the format the Operand is in by counting the commans present
+            int count = 0;
+
+            for (int i = 0; i < Operand.Length; i++)
+            {
+                if (Operand[i] == ',')
+                {
+                    count++;
+                }
+            }
+
+            if (Operand == " ")
+            {
+                return " ";
             }
             else
             {
-                format = "invalid";
+                switch (count)
+                {
+                    case 0:
+                        return "<label>";
+                    case 1:
+                        return "Rd, <memory ref>";
+                    case 2:
+                        return "Rd, Rn, <memory ref>";
+                    default:
+                        return "invalid";
+                }
             }
+        }
 
-            return format;
+        static string OpcodeFormat(string Opcode, Dictionary<string,string> instructionSet)
+        {
+            // uses a dictionary to return the format the operand should be in
+
+            if (instructionSet.ContainsKey(Opcode))
+            {
+                return instructionSet[Opcode];
+            }
+            else
+            {
+                return "invalid";
+            }
         }
 
         static string GetOperand(string instruction)
@@ -162,7 +155,7 @@ namespace NEA_CPU_Model
             // splits the operand from the instruction
             if (GetOpcode(instruction) == "HALT")
             {
-                operand = " ";
+                return " ";
             }
             else if (GetOpcode(instruction)[0] == 'B')
             {
@@ -194,28 +187,26 @@ namespace NEA_CPU_Model
 
         static string GetOpcode(string instruction)
         {
-            string opcode = string.Empty;
             // splits the opcode from the operand (but doesn't verify valid opcode)
             if (instruction == "HALT")
             {
-                opcode = "HALT";
+                return "HALT";
             }
             else if (instruction[0] == 'B')
             {
                 if (instruction[1] == ' ')
                 {
-                    opcode = "B";
+                    return "B";
                 }
                 else
                 {
-                    opcode = String.Concat(instruction[0], instruction[1], instruction[2], instruction[3], instruction[4]);
+                   return String.Concat(instruction[0], instruction[1], instruction[2], instruction[3], instruction[4]);
                 }
             }
             else
             {
-                opcode = String.Concat(instruction[0], instruction[1], instruction[2]);
+                return String.Concat(instruction[0], instruction[1], instruction[2]);
             }
-            return opcode;
         }
 
     }
