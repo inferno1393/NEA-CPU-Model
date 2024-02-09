7fe6d1f463532c58b222c7e7a25e67f48f5506f9
Rob Brown <rd_brown@btinternet.com>
2024-02-09 09:27:04 +0000

    Added comments

--- NEA CPU Model/Cache.cs
+++ NEA CPU Model/Cache.cs
@@ -13,10 +13,10 @@ namespace NEA_CPU_Model
     internal class Cache: AbstractMemory
     {
         // uses a dictionary to implement an associative array to store the cache values
-        Dictionary<string, int> cache = new Dictionary<string, int>() { };
+        Dictionary<string, int> cacheMemory = new Dictionary<string, int>() { };
 
         // uses a queue to tell which addresses have been in cache the longest
-        QueueArray<string> queue = new QueueArray<string>();
+        QueueArray<string> ageQueue = new QueueArray<string>();
 
         // attributes
         public int capacity = 4;
@@ -30,29 +30,31 @@ namespace NEA_CPU_Model
             }
             // updates specific purpose registers
             Program.model.marText.Text = address;
-            Program.model.mbrText.Text = cache[address].ToString();
+            Program.model.mbrText.Text = cacheMemory[address].ToString();
 
-            return cache[address]; // returns data
+            return cacheMemory[address]; // returns data
         }
 
         // stores the data given in the given address
         public override void StoreData(string address, int data)
         {
-            if (queue.Count == capacity)
+            if (ageQueue.Count == capacity)
             {
-                string addr = queue.Dequeue();
-                cache.Remove(addr);
+                string addr = ageQueue.Dequeue();
+                cacheMemory.Remove(addr);
             }
             if (IsAddressEmpty(address)) // checks if address is empty to avoid updating an existing key
             {
-                cache.Add(address, data);
-                
+                cacheMemory.Add(address, data); // address does not already exists so add the address
+                ageQueue.Enqueue(address); // adds the new address to the queue
             }
             else
             {
-                cache[address] = data;
+                cacheMemory[address] = data; // address already exsits so update the address
+                //ageQueue.Remove(address);
+                //ageQueue.Enqueue(address);
             }
-            queue.Enqueue(address); // adds the new address to the queue
+            
 
             // updates specific purpose registers
             Program.model.marText.Text = address;
@@ -64,7 +66,7 @@ namespace NEA_CPU_Model
         // checks if the given address is empty
         protected override bool IsAddressEmpty(string address)
         {
-            if (cache.ContainsKey(address)) // checks if key is in dictionary
+            if (cacheMemory.ContainsKey(address)) // checks if key is in dictionary
             {
                 return false;
             }
@@ -74,15 +76,15 @@ namespace NEA_CPU_Model
         // clears the current instance of cache
         public void Clear()
         {
-            cache = new Dictionary<string, int>() { };
-            queue = new QueueArray<string>();
+            cacheMemory = new Dictionary<string, int>() { }; // resets the cache memory
+            ageQueue = new QueueArray<string>(); // resets the age queue
         }
 
         // orders the addresses in cache in ascending numerical order and the displays them to the interface
         private void UpdateInterface()
         {
             // creates a dictionary containing the keys currently in cache
-            Dictionary<string, int>.KeyCollection keys = cache.Keys;
+            Dictionary<string, int>.KeyCollection keys = cacheMemory.Keys;
 
             // sorts keys into numerical order for visbility
             var sortedKeys = keys.OrderBy(key => Convert.ToInt32(key));
@@ -96,11 +98,11 @@ namespace NEA_CPU_Model
                 {
                     if (i < Model.cacheData.Count())
                     {   
-                        Model.cacheData[i].Text = cache[key.ToString()].ToString();
+                        Model.cacheData[i].Text = cacheMemory[key.ToString()].ToString();
                         Model.cacheAddress[i].Text = key.ToString();
                     }
                 }
-                i++;
+                i++; // increments the iteration value for next cycle
             }
         }
     }
